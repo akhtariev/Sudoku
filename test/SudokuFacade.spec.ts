fc2c4c3877d646f44b964519e8be22565e4b8505
Roman Akhtariev <ackhtariev@gmail.com>
2018-12-21 12:42:46 -0800

    Complete test suite

--- test/SudokuFacade.spec.ts
+++ test/SudokuFacade.spec.ts
@@ -63,7 +63,17 @@ describe("SudokuFacade solve with various Boards", () => {
             E, E, 3, 2, E, E, E, 8, E,
             E, 6, E, 5, E, E, E, E, 9,
             E, E, 4, E, E, E, E, 3, E,
-            E, E, E, E, E, 9, 7, E, E,]}
+            E, E, E, E, E, 9, 7, E, E,]},
+
+        emptyBoard:    {content: [E, E, E, E, E, E, E, E, E,   
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,
+                E, E, E, E, E, E, E, E, E,]}    
 
     };
 
@@ -80,9 +90,104 @@ describe("SudokuFacade solve with various Boards", () => {
         try {
             result = await sudokuFacade.solveBoard(toSolve);
             let length: number = toSolve.content.length;
-            expect(result.length).to.equal(length * length);
+            expect(result.content.length).to.equal(length * length);
             for(let cell of result)
-                expect(cell).to.be.true;
+                expect(cell).to.be.equal(E);
+            
+        } catch {
+            expect.fail("Not expected");
+        }
+
+    });
+
+    it("Should solve a valid easy board with one cell left", async () => {
+        let toSolve: Board = boards.easyGridLast;
+
+        let result: any;
+        try {
+            result = await sudokuFacade.solveBoard(toSolve);
+            let length: number = toSolve.content.length;
+            expect(result.content.length).to.equal(length * length);
+            for(let cell of result.content)
+                expect(cell).to.not.be.equal(E);
+            
+        } catch {
+            expect.fail("Not expected");
+        }
+
+    });
+
+    it("Should solve a very hard board", async () => {
+        let toSolve: Board = boards.veryHardGrid;
+
+        let result: any;
+        try {
+            result = await sudokuFacade.solveBoard(toSolve);
+            let length: number = toSolve.content.length;
+            expect(result.content.length).to.equal(length * length);
+            for(let cell of result.content)
+                expect(cell).to.not.equal(E);
+            
+        } catch {
+            expect.fail("Not expected");
+        }
+
+    });
+
+    it("Should not solve an invalid easy board", async () => {
+        let toSolve: Board = boards.invalidEasy;
+
+        let result: any;
+        try {
+            result = await sudokuFacade.solveBoard(toSolve);
+        } catch(err) {
+            result = err;
+        } finally {
+            expect(result).to.be.instanceOf(NotSolvedError);
+        }
+
+    });
+
+    it("Should not solve an invalid hard board", async () => {
+        let toSolve: Board = boards.invalidHard;
+
+        let result: any;
+        try {
+            result = await sudokuFacade.solveBoard(toSolve);
+        } catch(err) {
+            result = err;
+        } finally {
+            expect(result).to.be.instanceOf(NotSolvedError);
+        }
+
+    });
+
+    it("Should solve an empty board", async () => {
+        let toSolve: Board = boards.emptyBoard;
+
+        let result: any;
+        try {
+            result = await sudokuFacade.solveBoard(toSolve);
+            let length: number = toSolve.content.length;
+            expect(result.content.length).to.equal(length * length);
+            for(let cell of result.content)
+                expect(cell).to.not.equal(E);
+            
+        } catch {
+            expect.fail("Not expected");
+        }
+
+    });
+
+    it("Should produce different boards from initially empty grids", async () => {
+        let toSolve: Board = boards.emptyBoard;
+
+        let resultOne: any;
+        let resultTwo: any;
+        try {
+            resultOne = await sudokuFacade.solveBoard(toSolve);
+            resultTwo = await sudokuFacade.solveBoard(toSolve);
+            expect(resultOne.content).to.not.deep.equal(resultTwo.content);
             
         } catch {
             expect.fail("Not expected");
